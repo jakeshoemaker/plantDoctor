626daec1a0ba39172b275dbc944b72a79b2be15f
jakeshoemaker <jakeshoe3@gmail.com>
2020-07-31 00:01:12 -0500

    created api

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // create express app
 const express = require('express')
 const app = express()
+const db = require('./database.js');
 
 // server port
 const PORT = 1234
@@ -15,7 +16,38 @@ app.get("/", (req, res, next) => {
     res.json({"message": "Ok"})
 });
 
-// other endpoints here
+// get latest moisture level
+app.get('/api/moisture', (req, res, next) => {
+    var sql = "select * from moisture_level order by time desc limit 1";
+    var params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error": err.message});
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": rows
+        })
+    });
+});
+
+// get last hour of moisture
+app.get('/api/moisture/hr', (req, res, next) => {
+    var sql = "select * from moisture_level order by time desc limit 4";
+    var params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error": err.message});
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": rows
+        })
+    });
+});
+
 
 // default response to other requests
 app.use(function(req, res){
--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <p> test </p>
+      </div>
+      //<Container>
+        //  <Navbar expand="lg" variant="light" bg="light">
+          //  <Navbar.Brand href="#">Plant Donctor</Navbar.Brand>
+          //</Navbar>
+      //</Container>
+    );
+  }
 }
 
 export default App;
